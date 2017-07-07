0a0b79ab141cc62ad83476e91c220613055554bb
omsharama88 <om.sharma871@gmail.com>
2017-07-08 01:47:58 +0530

    initial

---
+++ src/Jenkins2.java
@@ -0,0 +1,9 @@
+
+public class Jenkins2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is jenkins demo 2");
+	}
+
+}
---
+++ src/jenkins1.java
@@ -0,0 +1,9 @@
+
+public class jenkins1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is jenkins git testing");
+	}
+
+}
