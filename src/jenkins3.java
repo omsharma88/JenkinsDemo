bc3257c687866b67c376a84c6e2e88f5a0304973
omsharama88 <om.sharma871@gmail.com>
2017-07-10 04:04:29 +0530

    tesyt

--- src/jenkins3.java
+++ src/jenkins3.java
@@ -4,6 +4,9 @@ public class jenkins3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is jenkins 3 file");
+		int value=5;
+		int tap=5/value;
+		System.out.println(tap);
 	}
 
 }
